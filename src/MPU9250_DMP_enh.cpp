9961ed78c82a44bf63fde75e849f186ddbf646bd
jhergeth <joachim@hergeth.name>
2018-07-21 19:03:29 +0200

    ...

--- src/MPU9250_DMP_enh.cpp
+++ src/MPU9250_DMP_enh.cpp
@@ -56,26 +56,8 @@ void MPU9250_DMP_enh::readMPU(){
     fmpuD[IMUm][0] = calcMag(mx); // magX is x-axis magnetic field in uT
     fmpuD[IMUm][1] = calcMag(my); // magY is y-axis magnetic field in uT
     fmpuD[IMUm][2] = calcMag(mz); // magZ is z-axis magnetic field in uT
-
-    // Must be called before updating quaternions!
-    updateTime();
-
-    // Sensors x (y)-axis of the accelerometer is aligned with the y (x)-axis of
-    // the magnetometer; the magnetometer z-axis (+ down) is opposite to z-axis
-    // (+ up) of accelerometer and gyro! We have to make some allowance for this
-    // orientationmismatch in feeding the output to the quaternion filter. For the
-    // MPU-9250, we have chosen a magnetic rotation that keeps the sensor forward
-    // along the x-axis just like in the LSM9DS0 sensor. This rotation can be
-    // modified to allow any convenient orientation convention. This is ok by
-    // aircraft orientation standards! Pass gyro rate as rad/s
-    //  MadgwickQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f,  my,  mx, mz);
-    MahonyQuaternionUpdate(fmpuD[IMUa][0], fmpuD[IMUa][1], fmpuD[IMUa][2],
-        fmpuD[IMUg][0], fmpuD[IMUg][1], fmpuD[IMUg][2],
-        fmpuD[IMUm][1], fmpuD[IMUm][0], fmpuD[IMUm][2],
-        deltat);
-
-        newData = true;
-    }
+    newData = true;
+}
 
 
 // Implementation of Sebastian Madgwick's "...efficient orientation filter
